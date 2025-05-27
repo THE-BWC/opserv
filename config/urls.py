1cb9bfbb777bb5f29d0f3eb3a3f195dd0d7bdec8
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2025-05-27 23:58:38 +0200

    Lot's of UI work and upcoming games now work.

--- config/urls.py
+++ config/urls.py
@@ -10,8 +10,10 @@ from drf_spectacular.views import SpectacularAPIView
 from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
+from .views import home
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", home, name="home"),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
---
+++ config/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from opserv.games import models as games_models
+
+
+def home(request):
+    """
+    Home view that renders the home page.
+
+    """
+    # Fetch all games that have upcoming operations
+    games = games_models.Game.objects.with_upcoming_operations()
+    # Each game in games will have a `.upcoming_operations` attribute
+    # (a list of upcoming operations)
+
+    # If no games are found, return an empty list
+    games = [] if not games else sorted(games, key=lambda game: game.name.lower())
+
+    # Render the home page with the list of games
+    return render(request, "pages/home.html", {"games": games})
