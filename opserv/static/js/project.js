376f810fb982d3a7221deb6faf4b2f4823d5d130
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2025-06-16 06:06:17 +0000

    Modified html templates to show allauth templates.

--- opserv/static/js/project.js
+++ opserv/static/js/project.js
@@ -4,12 +4,16 @@ import '../sass/project.scss';
 
 // This function displays the current time in EST (Eastern Standard Time) and updates it every second.
 function displayEstTime() {
-  document.getElementById('clock').innerHTML = Intl.DateTimeFormat('en', {
-    timeStyle: 'long',
-    hourCycle: 'h24',
-    timeZone: 'America/New_York',
-  }).format(new Date());
-  setTimeout(displayEstTime, 1000);
+  let clock = document.getElementById('clock');
+
+  if (clock !== null) {
+    clock.innerHTML = Intl.DateTimeFormat('en', {
+      timeStyle: 'long',
+      hourCycle: 'h24',
+      timeZone: 'America/New_York',
+    }).format(new Date());
+    setTimeout(displayEstTime, 1000);
+  }
 }
 
 // Initialize the clock display when the window loads
